2f8c5558126d2266855c4837ee75ad40d4050eda
poikilos <7557867+poikilos@users.noreply.github.com>
2022-05-25 11:06:40 -0400

    Make mobf optional for mob_environments so that the animalmaterials modpack can be used intact regardless of whether mobf is present.

--- mob_environments/init.lua
+++ mob_environments/init.lua
@@ -10,20 +10,24 @@
 --
 -- Contact sapier a t gmx net
 -------------------------------------------------------------------------------
-local ar_version = "0.0.1"
+if rawget(_G, "mobf") then
+    local ar_version = "0.0.1"
 
-core.log("action", "MOD: Loading mob_environments mod ...")
+    core.log("action", "MOD: Loading mob_environments mod ...")
 
---!path of mod
-local me_modpath = minetest.get_modpath("mob_environments")
+    --!path of mod
+    local me_modpath = minetest.get_modpath("mob_environments")
 
-dofile (me_modpath .. "/general_env_sets.lua")
-dofile (me_modpath .. "/flight_1.lua")
-dofile (me_modpath .. "/meadow.lua")
-dofile (me_modpath .. "/on_ground_2.lua")
-dofile (me_modpath .. "/open_waters.lua")
-dofile (me_modpath .. "/shallow_waters.lua")
-dofile (me_modpath .. "/deep_water.lua")
-dofile (me_modpath .. "/simple_air.lua")
+    dofile (me_modpath .. "/general_env_sets.lua")
+    dofile (me_modpath .. "/flight_1.lua")
+    dofile (me_modpath .. "/meadow.lua")
+    dofile (me_modpath .. "/on_ground_2.lua")
+    dofile (me_modpath .. "/open_waters.lua")
+    dofile (me_modpath .. "/shallow_waters.lua")
+    dofile (me_modpath .. "/deep_water.lua")
+    dofile (me_modpath .. "/simple_air.lua")
 
-core.log("action", "MOD: mob_environments mod " .. ar_version .. " loaded.")
+    core.log("action", "MOD: mob_environments mod " .. ar_version .. " loaded.")
+else
+    core.log("action", "MOD: mob_environments skipped (no mobf).")
+end
